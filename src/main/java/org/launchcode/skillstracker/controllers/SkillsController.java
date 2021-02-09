eb3dd2110e6dd24d0ca2b8d9a07984774116857c
Bre Bequette Reuther <67883123+BreBequette@users.noreply.github.com>
2021-02-08 20:15:09 -0600

    Finished studio and bonus missions

--- src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
+++ src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -27,45 +27,56 @@ public class SkillsController {
         String html;
         return "<html>" +
                     "<body>" +
-                        "<form action='form' method='post'>" +
-                        "<label for='name'>Name: </label><br>" +
-                        "<input id='name' type='text' name='name'/><br>" +
-                        "<label for='favorite-language'>My Favorite Language: </label><br>" +
-                             "<select id='favorite-language' name='language1'>" +
-                            "<option value='Java'>Java</option>" +
-                            "<option value='JavaScript'>JavaScript</option>" +
-                            "<option value='Python'>Python</option>" +
-                        "</select><br>" +
+                        "<form action='form-submit' method='post'>" +
+                            "<label for='name'>Name: </label><br>" +
+                            "<input id='name' type='text' name='name'/><br>" +
+                            "<label for='favorite-language'>My Favorite Language: </label><br>" +
+                            "<select id='favorite-language' name='language1'>" +
+                                "<option value='Java'>Java</option>" +
+                                "<option value='JavaScript'>JavaScript</option>" +
+                                "<option value='Python'>Python</option>" +
+                            "</select><br>" +
                             "<label for='second-favorite'>My Second Favorite Language: </label><br>" +
                             "<select id='second-favorite' name='language2'>" +
-                            "<option value='Java'>Java</option>" +
-                            "<option value='JavaScript'>JavaScript</option>" +
-                            "<option value='Python'>Python</option>" +
+                                "<option value='Java'>Java</option>" +
+                                "<option value='JavaScript'>JavaScript</option>" +
+                                "<option value='Python'>Python</option>" +
                             "</select><br>" +
-                        "<label for='third-favorite'>My Third Favorite Language: </label><br>" +
-                        "<select id='third-favorite' name='language3'>" +
-                        "<option value='Java'>Java</option>" +
-                        "<option value='JavaScript'>JavaScript</option>" +
-                        "<option value='Python'>Python</option>" +
-                        "</select><br>" +
-                        "<input type='submit' value='Submit'/>" +
+                            "<label for='third-favorite'>My Third Favorite Language: </label><br>" +
+                            "<select id='third-favorite' name='language3'>" +
+                                "<option value='Java'>Java</option>" +
+                                "<option value='JavaScript'>JavaScript</option>" +
+                                "<option value='Python'>Python</option>" +
+                            "</select><br>" +
+                            "<input type='submit' value='Submit'/>" +
                         "</form>" +
                     "</body>" +
                 "</html>";
     }
 
-    //@RequestMapping(value="form-submit", method={RequestMethod.GET, RequestMethod.POST})
-    @PostMapping("form")
+
+    //@PostMapping("form")
+    @RequestMapping(value="form-submit", method={RequestMethod.GET, RequestMethod.POST})
     public String formSubmission(@RequestParam String name, @RequestParam String language1, @RequestParam String language2, @RequestParam String language3 ){
+//        return "<html>" +
+//                "<body>" +
+//                "<h1>" + name + "</h1>" +
+//                "<ol>" +
+//                "<li>" + language1 + "</li>" +
+//                "<li>" + language2 + "</li>" +
+//                "<li>" + language3 + "</li>" +
+//                "</ol>" +
+//                "</body>" +
+//                "</html>";
         return "<html>" +
-                "<body>" +
-                "<h1>" + name + "</h1>" +
-                "<ol>" +
-                "<li>" + language1 + "</li>" +
-                "<li>" + language2 + "</li>" +
-                "<li>" + language3 + "</li>" +
-                "</ol>" +
-                "</body>" +
+                    "<body>" +
+                        "<table>" +
+                            "<tr><th><h1>" + name + "</h1></th><tr>" +
+                            "<tr><td>" + language1 + "</td></tr>" +
+                            "<tr><td>" + language2 + "</td></tr>" +
+                            "<tr><td>" + language3 + "</td></tr>" +
+                        "</table>" +
+                    "</body>" +
                 "</html>";
     }
 
